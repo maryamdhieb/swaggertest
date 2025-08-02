c67257d180987728d36aa69380ac77aac2b9f4e6
Maryam <maryamdhieb3@gmail.com>
2025-08-02 10:30:00 +0100

    upload

--- vite.config.js
+++ vite.config.js
@@ -2,33 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       '/api': {
-//         target: 'http://41.230.48.11:4800',
-//         changeOrigin: true,
-//         rewrite: (path) => path.replace(/^\/api/, ''),
-//         configure: (proxy, _options) => {
-//           proxy.on('proxyRes', (proxyRes, req, res) => {
-//             res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-//             res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS');
-//             res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//           });
-//         }
-//       }
-//     }
-//   }
-// })
 export default defineConfig({
+  plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://41.230.48.11:4800',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        configure: (proxy, _options) => {
+          proxy.on('proxyRes', (proxyRes, req, res) => {
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS');
+            res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+          });
+        }
       }
     }
   }
-});
+})
