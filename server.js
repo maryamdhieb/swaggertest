c166aef8a1beb8798610143c3a5719f75b1ef63d
Maryam <maryamdhieb3@gmail.com>
2025-08-02 12:34:51 +0100

    upload

--- server.js
+++ server.js
@@ -18,13 +18,3 @@ app.use('/api', createProxyMiddleware({
 
 // Servir le build React
 app.use(express.static(path.join(__dirname, 'dist')));
-
-// Catch all (React Router)
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
