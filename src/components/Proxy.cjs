d4e556f6a15edcc3e5105ab60fab897090e93288
Maryam <maryamdhieb3@gmail.com>
2025-08-04 12:52:42 +0100

    Update

--- src/components/Proxy.cjs
+++ src/components/Proxy.cjs
@@ -5,7 +5,7 @@ const app = express();
 
 // Middleware CORS
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://swaggertest-yw9i.onrender.com'); // Remplacez '*' par l’URL de votre frontend en production
+  res.setHeader('Access-Control-Allow-Origin', 'https://swaggertest1.onrender.com'); // Remplacez '*' par l’URL de votre frontend en production
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   if (req.method === 'OPTIONS') {
