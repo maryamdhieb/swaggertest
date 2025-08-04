90fe1f0b835c6c4420d32b7c9543db164f51d26a
Maryam <maryamdhieb3@gmail.com>
2025-08-04 10:51:22 +0100

    Update

--- Proxy.cjs
+++ Proxy.cjs
@@ -7,11 +7,11 @@ app.use('/api', createProxyMiddleware({
   target: 'http://41.230.48.11:4800',
   changeOrigin: true,
   pathRewrite: { '^/api': '' },
-  onProxyRes: function (proxyRes, req, res) {
-    res.setHeader('Access-Control-Allow-Origin', 'localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  }
+  // onProxyRes: function (proxyRes, req, res) {
+  //   res.setHeader('Access-Control-Allow-Origin', 'localhost:3000');
+  //   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS');
+  //   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  // }
 }));
 
 // app.listen(3000, () => console.log('Proxy running on http://localhost:3000'));
